6d5cf54a7568a9ea3c87d9f64ec2045d9e85205b
Hasan_Zarook <137222416+HasanZarook@users.noreply.github.com>
2025-09-04 16:15:56 +0500

    Add files via upload

---
+++ circle.java
@@ -0,0 +1,11 @@
+public class circle {
+    public static void main(String args[]){
+            circle no_of_c []= new circle[17];
+            for (int i=0 ; i< no_of_c.length ; i++){
+                no_of_c[i]= new circle();
+                System.out.println("Cirlce number :" + i);
+
+
+            }
+    }
+}
